5e3ed342d96da496116ca37d1c2e723ea10a78be
davy_chen <davy_chen@epam.com>
2021-06-21 10:53:55 +0800

    feat: ZHIHU-02 issue tracker 测试

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,10 +2,11 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'references-empty': [2, 'never'],
+    "subject-case": [0, "never"]
   },
   parserPreset: {
     parserOpts: {
-      issuePrefixes: ['zhihu#']
+      issuePrefixes: ['ZHIHU-']
     }
   }
 };
